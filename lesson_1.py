a4f03aaa9f1270ca0272abb7051e307a1cf4baeb
Дианка <fadeevad422@gmail.com>
2023-06-16 13:22:22 +0400

    first commit

---
+++ lesson_1.py
@@ -0,0 +1,8 @@
+# def strcounter(string):
+#     for symbol in set(string):
+#         counter = 0
+#         for sub_symbol in string:
+#          if symbol == sub_symbol:
+#               counter += 1
+#         print(symbol, counter)
+# strcounter('aaaabcddddd')
